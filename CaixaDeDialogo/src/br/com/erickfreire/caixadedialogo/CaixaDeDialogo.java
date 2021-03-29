e009c1a6a0a0d9bae7dceb64553c7fb73b7b7600
Erick Freire <erilefr@gmail.com>
2021-03-29 18:45:30 -0300

    CaixaDeDialogo
    CaixaDeDialogo

---
+++ CaixaDeDialogo/src/br/com/erickfreire/caixadedialogo/CaixaDeDialogo.java
@@ -0,0 +1,16 @@
+package br.com.erickfreire.caixadedialogo;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Programa em Java que cria uma caixa de diálogo
+ * @author Erick Freire
+ * @version 1 - Criado em 29-03-2021 as 18:44
+ */
+
+public class CaixaDeDialogo {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Bem vindo ao Java!");
+	}
+
+}
